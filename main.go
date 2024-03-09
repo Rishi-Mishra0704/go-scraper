5d4cab1c82518e99cd3796092ddfb48263c39f94
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2024-03-10 00:51:07 +0530

    add links in file

--- main.go
+++ main.go
@@ -2,31 +2,39 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/gocolly/colly"
 )
 
 func main() {
-	// Create a new Colly collector
+	file, err := os.Create("links.txt")
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+		return
+	}
+	defer file.Close()
+
 	c := colly.NewCollector(
-		// Visit only the domains example.com
 		colly.AllowedDomains("www.wikipedia.org"),
 	)
 
-	// Callback function to be executed when visiting each page
 	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
 		link := e.Attr("href")
+		text := e.Text
 		fmt.Println("Link found:", link)
-		fmt.Println("Text:", e.Text)
-		// Visit the link found recursively
+		fmt.Println("Text:", text)
+		_, err := file.WriteString(fmt.Sprintf("Link found: %s\nText: %s\n\n", link, text))
+		if err != nil {
+			fmt.Println("Error writing to file:", err)
+		}
+
 		e.Request.Visit(link)
 	})
 
-	// Callback function to be executed when the scraping is completed
 	c.OnScraped(func(r *colly.Response) {
 		fmt.Println("Finished scraping", r.Request.URL)
 	})
 
-	// Start scraping
 	c.Visit("https://www.wikipedia.org")
 }
